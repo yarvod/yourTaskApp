99479c261b7ee5ffa6725a8298b9d82657f698c5
yarvod <yar.vod.ole@gmail.com>
2023-03-01 00:08:38 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flatter/pages/home.dart';
 
 void main() => runApp(MaterialApp(
+      title: 'yourTask',
       theme: ThemeData(
         primaryColor: Colors.lightBlue,
       ),
